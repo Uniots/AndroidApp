1d26863dcf33229fac7a72b9ccee75581703031d
gl <danilasakhnov@gmail.com>
2023-08-28 21:17:03 +0700

    v 0.9 добавлены разделы в интервалах

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -12,11 +12,24 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import com.example.androidapp.ui.theme.AndroidAppTheme
 import com.example.androidapp.biblioteka.Biblioteka
+import com.example.androidapp.bolsekint.BolSekInt
+import com.example.androidapp.bolseptint.BolSeptInt
+import com.example.androidapp.bolsextaint.BolSextaInt
+import com.example.androidapp.boltercint.BolTercInt
+import com.example.androidapp.kvartaint.KvartaInt
+import com.example.androidapp.kvintaint.KvintaInt
 import com.example.androidapp.libraryintervali.LibraryIntervali
 import com.example.androidapp.librarytrezvucha.LibraryTrezvucha
 import com.example.androidapp.lybraryseptakkordi.LybrarySeptakkordi
+import com.example.androidapp.makseptint.MakSeptInt
+import com.example.androidapp.malsekint.MalSekInt
+import com.example.androidapp.malsextaint.MalSextaInt
+import com.example.androidapp.maltercint.MalTercInt
+import com.example.androidapp.oktava.Oktava
+import com.example.androidapp.primaint.PrimaInt
 import com.example.androidapp.test.Test
 import com.example.androidapp.tests.Tests
+import com.example.androidapp.tritonint.TritonInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,7 +66,20 @@ fun Start( ViewModel : MainViewModel){
     } else if (ScrNum.value == 2) {
         LibraryTrezvucha(onButtonendTapped = {ViewModel.setScreenNum(0)})
     } else if (ScrNum.value == 3) {
-        LibraryIntervali(onBattonbackTapped = {ViewModel.setScreenNum(0)})
+        LibraryIntervali(onBattonbackTapped = {ViewModel.setScreenNum(0)},
+            onPrimaTapped = {ViewModel.setScreenNum(11)},
+            onMalayaSecTapped = {ViewModel.setScreenNum(12)},
+            onBolSecTapped = {ViewModel.setScreenNum(13)},
+            onMalTercTapped = {ViewModel.setScreenNum(14)},
+            onBolTercTapped = {ViewModel.setScreenNum(15)},
+            onKvartaTapped = {ViewModel.setScreenNum(16)},
+            onTritonTapped = {ViewModel.setScreenNum(17)},
+            onKvintaTapped = {ViewModel.setScreenNum(18)},
+            onMalSextaTapped = {ViewModel.setScreenNum(19)},
+            onBolSextaTapped = {ViewModel.setScreenNum(20)},
+            onMalSeptTapped = {ViewModel.setScreenNum(21)},
+            onBolSeptTapped = {ViewModel.setScreenNum(22)},
+            onOktavaTapped = {ViewModel.setScreenNum(23)})
     } else if (ScrNum.value == 4){
         Tests(
             onLibrarymenuTapped = {ViewModel.setScreenNum(0)},
@@ -63,5 +89,31 @@ fun Start( ViewModel : MainViewModel){
             onIntervalitestTapped = {ViewModel.setScreenNum(5)})
     } else if (ScrNum.value == 5){
         Test(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 11){
+        PrimaInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 12){
+        MalSekInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 13){
+        BolSekInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 14){
+        MalTercInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 15){
+        BolTercInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 16){
+        KvartaInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 17){
+        TritonInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 18){
+        KvintaInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 19){
+        MalSextaInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 20){
+        BolSextaInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 21){
+        MakSeptInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 22){
+        BolSeptInt(onButtonBackTapped = {ViewModel.setScreenNum(4)})
+    }else if (ScrNum.value == 23){
+        Oktava(onButtonBackTapped = {ViewModel.setScreenNum(4)})
     }
 }
